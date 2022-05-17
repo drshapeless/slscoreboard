467908c7e00833e6f3b387f8c894155e710c5518
drshapeless <drsl@drshapeless.com>
2022-05-18 07:05:18 +0800

    Update main.

--- flutter_scoreboard/lib/main.dart
+++ flutter_scoreboard/lib/main.dart
@@ -2,94 +2,156 @@ import 'package:flutter/material.dart';
 import 'package:flutter_scoreboard/snookers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SnookerUploadApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SnookerUploadApp extends StatelessWidget {
+  const SnookerUploadApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Shit'),
+      title: "Snooker Upload",
+      theme: ThemeData.dark().copyWith(),
+      home: const SnookerUploadHome(title: "Snooker home"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class SnookerUploadHome extends StatefulWidget {
+  const SnookerUploadHome({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SnookerUploadHome> createState() => _SnookerUploadHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _usernameController1 = TextEditingController();
-  final TextEditingController _usernameController2 = TextEditingController();
-
-  final TextEditingController _passController = TextEditingController();
-
-  Future<Snooker>? _futureSnooker;
+class _SnookerUploadHomeState extends State<SnookerUploadHome> {
+  TextEditingController _usernameController1 = TextEditingController();
+  TextEditingController _usernameController2 = TextEditingController();
+  TextEditingController _passController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _buildPassTextField(),
-            // _buildUsernameRow(),
-          ],
-        ),
+      // backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          TextField(
+            controller: _passController,
+            decoration: InputDecoration(
+              labelText: "Pass",
+              prefixIcon: Icon(Icons.lock),
+            ),
+          ),
+          _buildPlayerTextfields(),
+          Padding(
+            padding: EdgeInsets.only(bottom: 10),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _build8balls(),
+              _build8balls(),
+            ],
+          ),
+          Divider(
+            color: Colors.white,
+          ),
+        ],
       ),
     );
   }
 
-  TextField _buildPassTextField() {
-    return TextField(
-      controller: _passController,
-      decoration: const InputDecoration(
-        hintText: "pass",
-        hintStyle: TextStyle(
-          color: Colors.grey,
-        ),
-      ),
-    );
-  }
-
-  Row _buildUsernameRow() {
+  Row _buildPlayerTextfields() {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TextField(
-          controller: _usernameController1,
-          decoration: const InputDecoration(
-            hintText: "Player 1",
-            hintStyle: TextStyle(
-              color: Colors.grey,
-            ),
-          ),
+        Expanded(
+          child: _buildPlayerTextfield1(_usernameController1, "Player 1"),
         ),
-        TextField(
-          controller: _usernameController2,
-          decoration: const InputDecoration(
-            hintText: "Player 2",
-            hintStyle: TextStyle(
-              color: Colors.grey,
-            ),
-          ),
+        Expanded(
+          child: _buildPlayerTextfield1(_usernameController2, "Player 2"),
         ),
       ],
+    );
+  }
+
+  TextField _buildPlayerTextfield1(TextEditingController con, String label) {
+    return TextField(
+      controller: con,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: const Icon(Icons.person),
+      ),
+    );
+  }
+
+  Column _build8balls() {
+    double padding = 20;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildBallButtonWithPadding("0", padding),
+            _buildBallButtonWithPadding("1", padding),
+            _buildBallButtonWithPadding("2", padding),
+            _buildBallButtonWithPadding("3", padding),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildBallButtonWithPadding("4", padding),
+            _buildBallButtonWithPadding("5", padding),
+            _buildBallButtonWithPadding("6", padding),
+            _buildBallButtonWithPadding("7", padding),
+          ],
+        )
+      ],
+    );
+  }
+
+  Padding _buildBallButtonWithPadding(String label, double padding) {
+    return Padding(
+      padding: EdgeInsets.all(padding),
+      child: _buildBallButton1(label),
+    );
+  }
+
+  MaterialColor c = Colors.yellow;
+
+  void changeColor(bool t) {
+    if (t) {
+      setState(() {
+        c = Colors.red;
+    });
+    } else {
+      setState(() {
+        c = Colors.yellow;
+    });
+    }
+  }
+
+  ElevatedButton _buildBallButton1(String label) {
+    return ElevatedButton(
+      onPressed: () {},
+      onHover: changeColor,
+      child: Text(
+        label,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: c,
+        shape: CircleBorder(),
+        padding: EdgeInsets.all(20),
+      ),
     );
   }
 }
